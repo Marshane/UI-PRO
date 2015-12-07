66c47e62b08549141b888072b238fa0752ad5a87
mazhengjie <462879619@qq.com>
2015-12-07 10:33:13 +0800

    'update'

--- ui-kit/lab/ui.postMessage.js
+++ ui-kit/lab/ui.postMessage.js
@@ -1,22 +1,52 @@
 angular.module('ui.postMessage',[])
-.factory("$postMessage", ['$rootScope',function($rootScope) {
-    var $messages=[];
-    return {
-        messages: function(msg) {
-            if (msg) {
-                $messages.push(msg);
-                $rootScope.$digest();
-            }
-            return $messages;
-        },
-        lastMessage: function() {
-            return $messages[$messages.length - 1];
-        },
-        post: function(msg, domain) {
-            if (!domain) {
-                domain = "*";
-            }
-            return $rootScope.$broadcast('$messageOutgoing', msg, domain);
+    .run(['$window', '$postMessage', '$rootScope', '$log', function($window, $postMessage, $rootScope, $log) {
+            $rootScope.$on('send', function(event, message, domain) {
+                var sender;
+                if (domain == null) {
+                    domain = "*";
+                }
+                sender = $rootScope.sender || $window.parent;
+                return sender.postMessage(message, domain);
+            });
+
+            angular.element($window).bind('message', function(event) {
+                var error, response;
+                event = event.originalEvent || event;
+                if (event && event.data) {
+                    response = null;
+                    $rootScope.sender = event.source;
+                    try {
+                        response = angular.fromJson(event.data);
+                    } catch (_error) {
+                        error = _error;
+                        $log.error('ahem', error);
+                        response = event.data;
+                    }
+                    response.origin = event.origin;
+                    $rootScope.$root.$broadcast('$receive', response);
+                    return $postMessage.messages(response);
+                }
+            });
         }
-    }
-}]);
+    ])
+    .factory("$postMessage", ['$rootScope',function($rootScope) {
+        var $messages=[];
+        return {
+            messages: function(msg) {
+                if (msg) {
+                    $messages.push(msg);
+                    $rootScope.$digest();
+                }
+                return $messages;
+            },
+            lastMessage: function() {
+                return $messages[$messages.length - 1];
+            },
+            post: function(msg, domain) {
+                if (!domain) {
+                    domain = "*";
+                }
+                return $rootScope.$broadcast('send', msg, domain);
+            }
+        }
+    }]);
