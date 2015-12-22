c5a687af2a8b63231e6fe4ee1a36f2053d1cb247
mazhengjie <mazhengjie@u51.com>
2015-12-22 19:02:49 +0800

    update

--- lab/ui.utils.js
+++
@@ -1,9 +0,0 @@
-angular.module('ui.utils',[
-    'ui.cut'
-]);
-angular.module('ui.cut',[])
-    .filter('cut',[function(){
-        return function(txt,num){
-            return String(txt).cut(num);
-        }
-    }]);
